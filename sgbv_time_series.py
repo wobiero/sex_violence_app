c0551932c097a78b08e26d130b4e594d7c5f5a43
wobiero <41239437+wobiero@users.noreply.github.com>
2022-01-04 17:31:48 -0500

    Update sgbv_time_series.py

--- sgbv_time_series.py
+++ sgbv_time_series.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from streamlit_disqus import st_disqus
-import matplotlib.pyplot as plt, mpld3
+import matplotlib.pyplot as plt
 import altair as alt
 import seaborn as sns
 
